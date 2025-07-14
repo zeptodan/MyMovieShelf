0ef3d881b08c6eb1611d71b7f0dbca85da6b96b9
Muhammad Anas <muhammadannas138@gmail.com>
2025-07-15 02:06:38 +0500

    added side navbar

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { useEffect,useState } from "react";
 import api from "../utilities/api";
 import Carousel from "../components/carousel";
+import SideNav from "../components/sidenav";
 const Home = () => {
   const [movies, setMovies] = useState({});
   useEffect(() => {
@@ -15,11 +16,16 @@ const Home = () => {
     getHome();
   }, []);
   return (
-    <div className="w-full">
-      <h1>Welcome to My Movie Shelf</h1>
-      <p>Your personal movie collection awaits!</p>
-      <Carousel movielist={movies.popular} />
-      <Carousel movielist={movies.top_rated} />
+    <div className="flex w-full h-full p-4">
+      <main className="w-full md:w-3/4">
+        <h1>Welcome to My Movie Shelf</h1>
+        <p>Your personal movie collection awaits!</p>
+        <Carousel movielist={movies.popular} />
+        <Carousel movielist={movies.top_rated} />
+      </main>
+      <aside className="w-1/4 hidden md:block">
+        <SideNav />
+      </aside>
     </div>
   );
 }
