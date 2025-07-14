6a4a8ef506b4698d17550b1d7747b04017f0deb1
Muhammad Anas <muhammadannas138@gmail.com>
2025-07-15 02:38:19 +0500

    added most of home page

--- Frontend/src/components/sidenav.jsx
+++ Frontend/src/components/sidenav.jsx
@@ -2,10 +2,10 @@ import { genres } from "../utilities/genres";
 const SideNav = () => {
     return (
         <div className="w-full p-4 rounded-lg bg-blue-950 flex flex-col items-center">
-                <a href="/movielist/popular" className='text-3xl font-bold my-4 hover:text-crimsonRed'>Popular</a>
-                <a href="/movielist/top_rated" className='text-3xl font-bold my-4 hover:text-crimsonRed'>Top Rated</a>
-                <h2 className='text-2xl font-bold my-4'>Genres</h2>
-                <ul className='text-xl gap-4 grid grid-cols-2 my-2'>
+                <a href="/movielist/popular" className='text-2xl lg:text-3xl font-bold my-4 hover:text-crimsonRed'>Popular</a>
+                <a href="/movielist/top_rated" className='text-2xl lg:text-3xl font-bold my-4 hover:text-crimsonRed'>Top Rated</a>
+                <h2 className='text-xl lg:text-2xl font-bold my-4'>Genres</h2>
+                <ul className='lg:text-xl gap-4 grid grid-cols-2 my-2'>
                     {genres.map((genre) => (
                         <li key={genre.id} className="flex justify-center">
                             <a href={`/movielist/${genre.id}`} className='hover:text-crimsonRed text-center'>{genre.name}</a>
--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -18,10 +18,27 @@ const Home = () => {
   return (
     <div className="flex w-full h-full p-4">
       <main className="w-full md:w-3/4">
-        <h1>Welcome to My Movie Shelf</h1>
-        <p>Your personal movie collection awaits!</p>
+        <h1 className="text-3xl mt-8 ml-8 font-bold">Welcome to My Movie Shelf</h1>
+        <p className="text-xl ml-12 pt-2 mb-8">Your personal movie collection awaits!</p>
+        <div className="flex justify-between items-center mx-4 mb-2 pb-2 border-b border-gray-300">
+          <h2 className="text-2xl ml-2 font-bold">Popular Movies</h2>
+          <a href="/movielist/popular" className="hover:text-crimsonRed text-xl">View More</a>
+        </div>
         <Carousel movielist={movies.popular} />
+        <div className="flex justify-between items-center mx-4 mb-2 pb-2 border-b border-gray-300">
+          <h2 className="text-2xl ml-2 font-bold">Top Rated Movies</h2>
+          <a href="/movielist/top_rated" className="hover:text-crimsonRed text-xl">View More</a>
+        </div>
         <Carousel movielist={movies.top_rated} />
+        <div className="flex justify-between items-center mx-4 mb-2 pb-2 border-b border-gray-300">
+          <h2 className="text-2xl ml-2 font-bold">Watchlist</h2>
+          <a href="/userlist/watchlist" className="hover:text-crimsonRed text-xl">View More</a>
+        </div>
+        {movies.watchlist.length > 0 ? <Carousel movielist={movies.watchlist} /> :
+        <div className="w-full h-52 flex items-center justify-center">
+          <p className="text-xl">WOW! So Empty</p>
+        </div>
+        }
       </main>
       <aside className="w-1/4 hidden md:block">
         <SideNav />
